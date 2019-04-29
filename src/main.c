71b80be9eb8ac1841e521058cafb62a5f97c0889
Arnauld Michelizza <arnauld.michelizza@ssi.gouv.fr>
2019-04-29 15:08:13 +0200

    indent

--- src/main.c
+++ src/main.c
@@ -10,7 +10,8 @@
 #include "libc/string.h"
 #include "libconsole.h"
 
-char buf[128] = { 0 };
+char    buf[128] = { 0 };
+
 /*
  * We use the local -fno-stack-protector flag for main because
  * the stack protection has not been initialized yet.
@@ -27,35 +28,35 @@ int _main(uint32_t task_id)
     printf("smart is task %x !\n", id);
 
     ret = console_early_init(4, 115200);
-    printf("Registered USART through libconsole. Returns %s !\n", strerror(ret));
+    printf("Registered USART through libconsole. Returns %s !\n",
+           strerror(ret));
 
     ret = sys_init(INIT_GETTASKID, "crypto", &id_crypto);
     printf("crypto is task %x !\n", id_crypto);
 
-
-
     ret = sys_init(INIT_DONE);
-//    ret = sys_ipc(IPC_SEND_SYNC, 1, 15, buffer_out); // to sdio
     printf("sys_init DONE returns %x !\n", ret);
 
     ret = console_init();
     if (ret == 0) {
-      printf("USART4 is now configured !\n");
+        printf("USART4 is now configured !\n");
     } else {
-      printf("error during configuration of USART3\n");
+        printf("error during configuration of USART3\n");
     }
 
     printf("sending welcome msg to UART4\n");
-    console_log("[USART%x initialized for console output, baudrate=%x]\n", 4, 115200);
+    console_log("[USART%x initialized for console output, baudrate=%x]\n", 4,
+                115200);
     while (1) {
         logsize_t size = 128;
+
         id = id_crypto;
         ret = sys_ipc(IPC_RECV_SYNC, &id, &size, buf);
         if (ret == SYS_E_INVAL) {
             printf("error: recv_sync() returns SYS_E_INVAL\n");
         }
         if (ret == SYS_E_DONE) {
-          console_log(buf);
+            console_log(buf);
         }
     }
     printf("welcome msg sent\n");
@@ -63,6 +64,6 @@ int _main(uint32_t task_id)
     while (1) {
         sys_yield();
     }
-    /* should return to do_endoftask() */
+
     return 0;
 }
